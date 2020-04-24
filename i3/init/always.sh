d4c5f8a73db86a1dc6e028f992dcf0b727553120
Alistair McIntyre <alistairmcintyre@catalyst.net.nz>
2020-04-25 11:54:26 +1200

    Update always.sh script to work correctly

--- i3/init/always.sh
+++ i3/init/always.sh
@@ -8,8 +8,15 @@ fi
 
 function stop_proc() {
     killall -q $1
-    while pgrep -x $1 >/dev/null; do sleep 0.5; done
+    while pgrep -x $1 >/dev/null; do sleep 1; done
 }
+
+# i3-workspace-names-daemon applies FontAwesome icons to WS names
+i3-workspace-names-daemon --delimiter " " &
+# Set my gnome configuration (GTK)
+/usr/lib/gnome-settings-daemon/gsd-xsettings &
+# Set wallpaper
+feh --bg-fill ~/Pictures/Wallpapers/$(ls ~/Pictures/Wallpapers/ | shuf -n 1) &
 
 stop_proc compton
 compton &
@@ -17,16 +24,9 @@ compton &
 stop_proc dunst
 dunst &
 
-echo $POLYBAR_ELEMENTS >> ~/.testfile
-
 stop_proc polybar
 polybar $HOSTNAME &
 
-# i3-workspace-names-daemon applies FontAwesome icons to WS names
-i3-workspace-names-daemon --delimiter " "
-# Set my gnome configuration (GTK)
-/usr/lib/gnome-settings-daemon/gsd-xsettings
-# Set wallpaper
-feh --bg-fill ~/Pictures/Wallpapers/$(ls ~/Pictures/Wallpapers/ | shuf -n 1)
+
 
 
