0d05d0546cb2accec0a20fece9450fa8e8c7a5c3
Alistair McIntyre <alistairmcintyre@catalyst.net.nz>
2020-05-11 21:46:12 +1200

    Laptop updates

--- i3/init/hosts/alistairmcintyre-lp/always.sh
+++ i3/init/hosts/alistairmcintyre-lp/always.sh
@@ -5,9 +5,9 @@ DIMENSIONS=$(xdpyinfo  | grep -oP 'dimensions:\s+\K\S+')
 
 if [ $DIMENSIONS = '1920x1080' ]
 then
-  export POLYBAR_ELEMENTS="vpn vpn-icon spacer wifi wifi-icon spacer date date-icon"
+  export POLYBAR_ELEMENTS="vpn vpn-icon spacer wifi wifi-icon spacer date date-icon spacer"
 elif [ $DIMENSIONS = '3440x1440' ]
 then
-  export POLYBAR_ELEMENTS="vpn vpn-icon spacer wifi wifi-icon spacer memory memory-icon spacer cpu cpu-icon spacer date date-icon"
+  export POLYBAR_ELEMENTS="vpn vpn-icon spacer wifi wifi-icon spacer memory memory-icon spacer cpu cpu-icon spacer date date-icon spacer"
 fi
 
